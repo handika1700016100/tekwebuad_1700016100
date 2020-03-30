907718d0e81de62ca646ea935f258e7d5b2cd262
handika1700016100 <handika17000016100@webmail.uad.ac.id>
2020-03-30 22:30:23 +0700

    Navigasi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Praktikum1Component } from './pages/praktikum1/praktikum1.component';
+import { Praktikum2Component } from './pages/praktikum2/praktikum2.component';
+import { Praktikum3Component } from './pages/praktikum3/praktikum3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Praktikum1Component,
+    Praktikum2Component,
+    Praktikum3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
